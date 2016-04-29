64fd9e1dd2127d226af8f9da658351715fbdd430
Benoit Marcot <juz4m@hotmail.com>
2016-04-29 17:00:58 +0200

    timer_2: Create 4 threads, each thread creates a timer.

---
+++ timer_2/main.c
@@ -0,0 +1,33 @@
+#include "kernel.h"
+#include "pthread.h"
+#include "unit.h"
+
+int vals[] = {0, 0, 0, 0};
+
+void *fn(void *arg)
+{
+	msleep(500 + (int) arg * 500);
+	vals[(int) arg]++;
+	printk("Thread_%d returns.\n", (int) arg);
+
+	return 0;
+}
+
+int main()
+{
+	pthread_t tips[4];
+
+	for (int i = 0; i < 4; i++) {
+		if (pthread_create(&tips[i], fn, (void *) i)) {
+			printk("failed: can't create new posix thread.\n");
+			TEST_EXIT(1);
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		while (vals[i] == 0)
+			pthread_yield();
+	}
+
+	TEST_EXIT(0);
+}
