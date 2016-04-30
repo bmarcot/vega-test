bfca6dd85e08f2a38675d9b15ab42b52b9388785
Benoit Marcot <juz4m@hotmail.com>
2016-04-30 13:14:58 +0200

    mm_1: Add simple allocate/free memory test.

---
+++ mm_1/main.c
@@ -0,0 +1,17 @@
+#include "mm.h"
+#include "unit.h"
+
+int main()
+{
+	void *p, *q;
+
+	if ((p = page_alloc(256)) == NULL)
+		TEST_EXIT(1);
+	page_free(p);
+
+	/* same page should be reallocated */
+	if ((q = page_alloc(256)) != p)
+		TEST_EXIT(1);
+
+	TEST_EXIT(0);
+}
