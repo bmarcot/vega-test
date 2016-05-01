dd2939421f79e59ec862a74cb3af4a7c38a8fb68
Benoit Marcot <juz4m@hotmail.com>
2016-05-01 11:51:18 +0200

    (minor) mm_2: Revisit prints.

--- mm_2/main.c
+++ mm_2/main.c
@@ -4,11 +4,11 @@
 int main()
 {
 	void *p[14];
-	int sz[] = {256, 1024, 2048, 256, 256, 256, 512, 512, 1024, 512, 2048,
-		    256, 512, 1024};
+	int sz[] = {256, 1024, 2048, 256, 256, 256, 512, 512, 1024, 512,
+		     2048, 256, 512, 1024};
 
 	for (int j = 0; j < 30; j++) {
-		printk("iteration %d\n", j);
+		printk("Iteration #%d\n", j);
 		for (int i = 0; i < 14; i++) {
 			if ((p[i] = page_alloc(sz[i])) == NULL)
 				TEST_EXIT(1);
