bc013b7395e7903b15d9bb6b1d6acc0468b2386e
Benoit Marcot <juz4m@hotmail.com>
2016-05-01 11:22:52 +0200

    mm_2: Allocate and free multiple sizes.

---
+++ mm_2/main.c
@@ -0,0 +1,20 @@
+#include "mm.h"
+#include "unit.h"
+
+int main()
+{
+	void *p[14];
+	int sz[] = {256, 1024, 2048, 256, 256, 256, 512, 512, 1024, 512, 2048,
+		    256, 512, 1024, 256, 512};
+
+	for (int j = 0; j < 30; j++) {
+		for (int i = 0; i < 16; i++) {
+			if ((p[i] = page_alloc(sz[i])) == NULL)
+				TEST_EXIT(1);
+		}
+		for (int i = 0; i < 16; i++)
+			page_free(p[i]);
+	}
+
+	TEST_EXIT(0);
+}
