41e0581b3bd5d5eccfc1bcca094d4cd98a406855
Benoit Marcot <juz4m@hotmail.com>
2016-05-03 10:17:19 +0200

    mutex_2: Fix test, check thread waited.

--- mutex_2/main.c
+++ mutex_2/main.c
@@ -4,12 +4,14 @@
 #include "unit.h"
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int has_waited;
 
 void *fn(__unused void *arg)
 {
 	printk("thread 2: acquire mutex...\n");
 	if (pthread_mutex_lock(&lock))
 		return NULL;
+	has_waited = 1;
 	printk("thread 2: OK, mutex locked...\n");
 	if (pthread_mutex_unlock(&lock))
 		return NULL;
@@ -29,6 +31,8 @@ int main(void)
 		TEST_EXIT(1);
 	printk("thread 1: mutex locked, yield now...\n");
 	pthread_yield();
+	if (has_waited)
+		TEST_EXIT(1);
 	printk("thread 1: return from yield.\n");
 	if (pthread_mutex_unlock(&lock))
 		TEST_EXIT(1);
