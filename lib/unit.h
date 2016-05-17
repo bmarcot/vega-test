0d637d3675e5c8176060fd8e548537ef2162a30d
Benoit Marcot <juz4m@hotmail.com>
2016-05-17 20:20:31 +0200

    Revisit the lib unit, print test result in color.

--- lib/unit.h
+++ lib/unit.h
@@ -1,27 +1,38 @@
-#ifndef TESTS_LIB_UNIT_H
-#define TESTS_LIB_UNIT_H
+#ifndef TEST_LIB_UNIT_H
+#define TEST_LIB_UNIT_H
 
 #include "kernel.h"
+
+#define C_GREEN	 "\033[1;32;40m"
+#define C_RED    "\033[1;31;40m"
+#define C_NORMAL "\033[0m"
+
+#define printk_green(format, ...)			\
+	printk(C_GREEN format C_NORMAL, ##__VA_ARGS__);
+
+#define printk_red(format, ...)				\
+	printk(C_RED format C_NORMAL, ##__VA_ARGS__);
+
 
 #ifdef CONFIG_SEMIHOSTING
 
 void semih_exit(int);
 
-#define TEST_EXIT(ret_val) ({				  \
-	if (ret_val)					  \
-		printk("test failed: %d\n", (ret_val));	  \
-	else						  \
-		printk("test passed\n");		  \
+#define TEST_EXIT(ret_val) ({			       \
+	if (ret_val) {				       \
+		printk_red("test failed: %d\n", (ret_val));	\
+	} else {				       \
+		printk_green("test passed\n"); }       \
 	semih_exit(ret_val); })
 
 #else
 
-#define TEST_EXIT(ret_val) ({				  \
-	if (ret_val)					  \
-		printk("test failed: %d\n", (ret_val));	  \
-	else						  \
-		printk("test passed\n");		  \
+#define TEST_EXIT(ret_val) ({			       \
+	if (ret_val) {				       \
+		printk_red("test failed: %d\n", (ret_val));	\
+	} else {				       \
+		printk_green("test passed\n"); }       \
 	return (ret_val); })
 #endif
 
-#endif /* !TESTS_LIB_UNIT_H */
+#endif /* !TEST_LIB_UNIT_H */
