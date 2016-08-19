e9959d6854a04f867bff01c83cf5df3a230d51e6
Benoit Marcot <juz4m@hotmail.com>
2016-08-19 16:47:48 +0200

    mm_2: Use the new page allocator

--- mm_2/main.c
+++ mm_2/main.c
@@ -1,20 +1,22 @@
-#include <kernel/mm.h>
+#include <stddef.h>
+
+#include <kernel/page.h>
 #include "unit.h"
 
 int main()
 {
 	void *p[14];
 	int sz[] = {256, 1024, 2048, 256, 256, 256, 512, 512, 1024, 512,
-		     2048, 256, 512, 1024};
+		    2048, 256, 512, 1024};
 
 	for (int j = 0; j < 30; j++) {
 		printk("Iteration #%d\n", j);
 		for (int i = 0; i < 14; i++) {
-			if ((p[i] = page_alloc(sz[i])) == NULL)
+			if ((p[i] = alloc_pages(size_to_page_order(sz[i]))) == NULL)
 				TEST_EXIT(1);
 		}
 		for (int i = 0; i < 14; i++)
-			page_free(p[i]);
+			free_pages((unsigned long)p[i], size_to_page_order(sz[i]));
 	}
 
 	TEST_EXIT(0);
