36edc3ee40c6b7cd1579fbdfb3ece2f33ff228eb
Benoit Marcot <juz4m@hotmail.com>
2016-10-11 16:24:15 +0200

    msleep_2: Refactor, fix warnings

--- msleep_2/main.c
+++ msleep_2/main.c
@@ -1,17 +1,17 @@
 // simple thread create and thread yield
 
+#include <pthread.h>
+#include <stddef.h>
 #include "kernel.h"
-#include "pthread.h"
 #include "unit.h"
-#include "linux/stddef.h"
+
+extern void msleep(unsigned int);
 
 static int val;
 
 static void *fn(void *arg)
 {
-	(void)arg;
-
-	msleep(600);
+	msleep(300 * (1 + (int)arg));
 	val++;
 
 	return 0;
@@ -22,16 +22,13 @@ int main()
 	pthread_t tip;
 
 	for (int i = 0; i < 3; i++) {
-		if (pthread_create(&tip, NULL, fn, NULL)) {
+		if (pthread_create(&tip, NULL, fn, (void *)i)) {
 			printk("failed: can't create new posix thread.\n");
 			TEST_EXIT(1);
 		}
 	}
-
-	msleep(600);  /* 4 threads are sitting in the sleepqueue */
-
 	while (val != 3)
-		;
+		pthread_yield();
 
 	TEST_EXIT(0);
 }
