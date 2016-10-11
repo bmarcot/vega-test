8d9bb35a2033c7c9193488f9e8c38622671fa50e
Benoit Marcot <juz4m@hotmail.com>
2016-10-11 22:06:35 +0200

    timer_1: Test POSIX timers

--- timer_1/main.c
+++ timer_1/main.c
@@ -1,11 +1,33 @@
 #include "unit.h"
 #include <kernel/timer.h>
+#include <kernel/time.h>
+#include <kernel/signal.h>
+
+#define EXPECTED_VALUE  0xabadcafeul
+
+int received_signal;
+
+static void event(union sigval sival)
+{
+	if (sival.sival_int != (int)EXPECTED_VALUE) {
+		printk("error: Did not received expected value (%x != %x)\n",
+			sival.sival_int, EXPECTED_VALUE);
+		TEST_EXIT(1);
+	}
+	received_signal = 1;
+}
 
 int main()
 {
-	printk("Create a 2s timer...\n");
-	timer_create(2000);
-	printk("OK, timer expired!\n");
+	struct sigevent sevp = { .sigev_notify_function = event,
+				 .sigev_value.sival_int = EXPECTED_VALUE };
+	timer_t timerid;
+
+	sys_timer_create(1, &sevp, &timerid);
+	timer_settime(timerid, 0, 1000);
+
+	while (!received_signal)
+		;
 
 	TEST_EXIT(0);
 }
