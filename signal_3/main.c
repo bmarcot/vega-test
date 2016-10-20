f0557a0e515124a32c2464999bc96303e62b4a7a
Benoit Marcot <juz4m@hotmail.com>
2016-10-20 15:21:36 +0200

    signal_3: Fix errno

--- signal_3/main.c
+++ signal_3/main.c
@@ -1,6 +1,8 @@
 /* test signal error return code */
 
+#include <errno.h>
 #include <signal.h>
+#include <kernel/errno-base.h>
 #include <kernel/signal.h>
 #include "kernel.h"
 #include "unit.h"
@@ -16,15 +18,15 @@ int main(void *arg)
 
 	/* test ERR_SIGNAL_UNSUPPORTED */
 	retval = sigqueue(0, SIGMAX + 1, value);
-	if (retval != ERR_SIGNAL_UNSUPPORTED) {
-		printk("error: expected UNSUPPORTED_SIGNAL retcode\n");
+	if (retval != -1 || errno != EINVAL) {
+		printk("error: expected EINVAL\n");
 		TEST_EXIT(1);
 	}
 
 	/* test ERR_SIGNAL_UNHANDLED */
 	retval = sigqueue(0, SIGUSR1, value);
-	if (retval != ERR_SIGNAL_UNHANDLED) {
-		printk("error: expected UNHANDLED_SIGNAL retcode\n");
+	if (retval != -1 || errno != EINVAL) {
+		printk("error: expected EINVAL\n");
 		TEST_EXIT(1);
 	}
 
