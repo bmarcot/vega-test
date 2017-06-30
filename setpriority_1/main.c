91610fc790b627a1cfdd7c3d996d879de9b72497
Benoit Marcot <benoit.marcot@nordicsemi.no>
2017-06-30 16:33:28 +0200

    setpriority_1: Simple task priority test

---
+++ setpriority_1/main.c
@@ -0,0 +1,21 @@
+/* simple test that sets task priority */
+
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "unit.h"
+
+#define PRIO_PROCESS 0
+
+int main()
+{
+	int pid = getpid();
+
+	if (setpriority(PRIO_PROCESS, pid, 16))
+		TEST_EXIT(1);
+	if (getpriority(PRIO_PROCESS, pid) != 16)
+		TEST_EXIT(1);
+
+	TEST_EXIT(0);
+}
