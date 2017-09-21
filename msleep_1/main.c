67fd7c84f3ce0118267cbc153b1e75a2ffbcc8a5
Benoit Marcot <benoit.marcot@nordicsemi.no>
2017-09-21 14:03:23 +0200

    msleep_1: Change delay

--- msleep_1/main.c
+++ msleep_1/main.c
@@ -5,9 +5,9 @@ extern void msleep(unsigned int);
 
 int main()
 {
-	for (int i = 1; i < 6; i++) {
-		printk("%d...", i);
-		msleep(1000);
+	for (int i = 0; i <= 13; i++) {
+		printk("%d...\n", i);
+		msleep(700);
 	}
 	printk("\n");
 
