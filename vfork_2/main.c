a6916c834cbee2b4c3a62b0378f1aec438345118
Benoit Marcot <benoit.marcot@nordicsemi.no>
2017-10-20 15:59:10 +0200

    vfork_2: Wait for child process

--- vfork_2/main.c
+++ vfork_2/main.c
@@ -1,10 +1,11 @@
 /* calling vfork() multiple times won't deplete the memory */
 
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "syscall-wrappers.h"
-
 #include "unit.h"
 
 int main(void)
@@ -12,12 +13,14 @@ int main(void)
 	pid_t pid;
 	static int i = 0;
 
-	while (i < 100000) {
+	while (i < 1000000) {
 		pid = vfork();
-		if (!pid)
-			_exit(0);
-		else
+		if (!pid) {
+			_Exit(0);
+		} else {
+			waitpid(pid, NULL, 0);
 			i++;
+		}
 	}
 
 	TEST_EXIT(0);
