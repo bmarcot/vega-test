a15c124ce7d6f84ddf760614fab056f3288c9659
Benoit Marcot <benoit.marcot@nordicsemi.no>
2017-10-20 15:58:54 +0200

    vfork_1: Wait for child process

--- vfork_1/main.c
+++ vfork_1/main.c
@@ -1,10 +1,11 @@
 /* simple vfork() test */
 
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "syscall-wrappers.h"
-
 #include "unit.h"
 
 int main(void)
@@ -20,9 +21,11 @@ int main(void)
 		/* child should exit before its parent */
 		if (!count)
 			TEST_EXIT(1);
-		TEST_EXIT(0);
+		waitpid(pid, NULL, 0);
 	} else {
 		count++;
 		_Exit(0);
 	}
+
+	TEST_EXIT(0);
 }
