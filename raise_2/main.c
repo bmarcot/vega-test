e5d989141d98e9b89ed0b90bcfd6bb019325e845
Benoit Marcot <benoit.marcot@nordicsemi.no>
2017-11-03 16:53:23 +0100

    raise_2: Signal with no handler is allowed

--- raise_2/main.c
+++ raise_2/main.c
@@ -1,20 +1,12 @@
-/* test raise() return code */
+/* raise a signal that has no handler is OK */
 
-#include <stddef.h>
 #include <signal.h>
 
-#include <kernel/errno-base.h>
-#include "kernel.h"
 #include "unit.h"
 
-int main(void *arg)
+int main()
 {
-	(void) arg;
-
-	sigaction(SIGUSR1, NULL, NULL); /* shall return -EINVAL */
-	int retval = raise(0);
-	printk("Got return value %d (negative)\n", -retval);
-	if (retval != -EINVAL)
+	if (raise(SIGUSR1))
 		TEST_EXIT(1);
 
 	TEST_EXIT(0);
