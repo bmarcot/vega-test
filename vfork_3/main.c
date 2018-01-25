56d8e5c7fde6832546990eb03e6dd6cabb05aecd
Benoit Marcot <benoit.marcot@nordicsemi.no>
2018-01-25 15:50:56 +0100

    vfork_3: Check SIGCHLD info fields

--- vfork_3/main.c
+++ vfork_3/main.c
@@ -7,31 +7,48 @@
 #include "syscall-wrappers.h"
 #include "unit.h"
 
-static int val;
+#define EXIT_CODE 123
 
-static void sigchld_handler(int sig)
+static int val;
+static pid_t childpid;
+
+static void sigchld_handler(int sig, siginfo_t *info, void *unused)
 {
 	if (sig != SIGCHLD) {
-		printk("Received wrong signal\n");
+		printk("Expect SIGCHLD\n\n");
 		TEST_EXIT(1);
 	}
+	if (info->si_signo != SIGCHLD) {
+		printk("Wrong signal number (%d)\n", info->si_signo);
+		TEST_EXIT(1);
+	}
+	if (info->si_code != CLD_EXITED) {
+		printk("Wrong signal code (%d)\n", info->si_code);
+		TEST_EXIT(1);
+	}
+	if (info->_sigchld.si_status != EXIT_CODE) {
+		printk("Wrong status value (%d)\n", info->_sigchld.si_status);
+		TEST_EXIT(1);
+	}
+	childpid = info->_sigchld.si_pid;
 	val++;
 }
 
 int main(void)
 {
-	pid_t pid;
-	struct sigaction act;
-
-	act.sa_handler = sigchld_handler;
-	act.sa_flags = 0;
+	const struct sigaction act = { .sa_sigaction = sigchld_handler,
+				       .sa_flags = SA_SIGINFO };
 	sigaction(SIGCHLD, &act, NULL);
 
-	pid = vfork();
+	pid_t pid = vfork();
 	if (!pid) {
-		_Exit(0);
+		_Exit(EXIT_CODE);
 	} else {
 		waitpid(pid, NULL, 0);
+		if (pid != childpid) {
+			printk("Wrong child PID (%d != %d)\n", pid, childpid);
+			TEST_EXIT(1);
+		}
 		if (!val) {
 			printk("Signal not received\n");
 			TEST_EXIT(1);
