a78130a9756684dc6fb8af188645170b6e5a7103
Benoit Marcot <benoit.marcot@nordicsemi.no>
2018-02-12 16:37:57 +0100

    exit_1: Can exit process from non-leader thread

---
+++ exit_1/main.c
@@ -0,0 +1,38 @@
+/* exit_group() can exit process from non-leader thread */
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "syscall-wrappers.h"
+#include "unit.h"
+
+int pause(void);
+
+static void *start0(void *arg)
+{
+	_Exit(42);
+}
+
+int main(void)
+{
+	int status;
+	pid_t pid;
+	pthread_t thread0;
+
+	pid = vfork();
+	if (pid > 0) {
+		if (waitpid(pid, &status, 0) == -1)
+			TEST_EXIT(1);
+		if (status != 42)
+			TEST_EXIT(1);
+	} else {
+		if (pthread_create(&thread0, NULL, start0, NULL))
+			TEST_EXIT(1);
+		pause();
+		_Exit(0);
+	}
+
+	TEST_EXIT(0);
+}
