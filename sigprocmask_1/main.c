fec17b5a5a2ba624fc9afbe3c629f4240b0e417f
Benoit Marcot <juz4m@hotmail.com>
2018-03-12 22:22:33 +0100

    sigprocmask_1: Test blocked signal, unblocking

---
+++ sigprocmask_1/main.c
@@ -0,0 +1,45 @@
+/* test blocked signal, test unblocking pending signal */
+
+#include <signal.h>
+#include <unistd.h>
+
+#include <kernel/signal.h> //XXX: sigaddset()
+
+#include "syscall-wrappers.h"
+#include "unit.h"
+
+static int val;
+
+static void handler(int sig)
+{
+	val++;
+}
+
+int main(void)
+{
+	struct sigaction act;
+
+	act.sa_handler = handler;
+	act.sa_flags = 0;
+	sigaction(SIGUSR1, &act, NULL);
+
+	sigset_t m;
+	sigaddset(&m, SIGUSR1);
+
+	// blocked signals are pending
+	sigprocmask(SIG_BLOCK, &m, NULL);
+	raise(SIGUSR1);
+	if (val) {
+		printk("SIGUSR1 is blocked\n");
+		TEST_EXIT(1);
+	}
+
+	// unblocked pending signals are taken
+	sigprocmask(SIG_UNBLOCK, &m, NULL);
+	if (!val) {
+		printk("SIGUSR1 is unblocked\n");
+		TEST_EXIT(1);
+	}
+
+	TEST_EXIT(0);
+}
