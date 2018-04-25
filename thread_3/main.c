3238d4cf39633f197b6676856574a42e34124826
Benoit Marcot <benoit.marcot@nordicsemi.no>
2018-04-25 09:27:45 +0200

    thread_3: Create 24 threads, then join those threads

--- thread_3/main.c
+++ thread_3/main.c
@@ -1,40 +1,40 @@
+/* create THREAD_COUNT threads, then join those threads */
+
 #include <pthread.h>
-#include <sched.h>
-
-#include <kernel/kernel.h>
-
 #include "unit.h"
 
-static int count;
+#define THREAD_COUNT 24
+
+static int counter;
 
 void *fn(void *arg)
 {
-	(void) arg;
-	printk("counter=%d\n", count);
-	count++;
+	counter++;
 
 	return 0;
 }
 
 int main()
 {
-	pthread_t tips[25];
+	pthread_t tips[THREAD_COUNT];
 
-	/* reduce the thread stack size to 256 bytes */
+	/* reduce stack size to 256 bytes */
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setstacksize(&attr, 256);
 
-	/* create 25 threads */
-	for (int i = 0; i < 25; i++) {
+	for (int i = 0; i < THREAD_COUNT; i++) {
 		if (pthread_create(&tips[i], &attr, fn, NULL)) {
 			printk("failed: can't create new posix thread.\n");
 			TEST_EXIT(1);
 		}
 	}
 
-	while (count < 24)
-		sched_yield();
+	for (int i = 0; i < THREAD_COUNT; i++)
+		pthread_join(tips[i], NULL);
+
+	if (counter != THREAD_COUNT)
+		TEST_EXIT(1);
 
 	TEST_EXIT(0);
 }
