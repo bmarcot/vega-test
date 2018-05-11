a87d15650d742f18e5334f5e9c5d0e9ebaa2556b
Benoit Marcot <benoit.marcot@nordicsemi.no>
2018-05-11 15:53:35 +0200

    hrtimer_2: Fixup test following hrtimer rewrite

--- hrtimer_2/main.c
+++ hrtimer_2/main.c
@@ -1,41 +1,42 @@
 /* test concurrent high-resolution timers */
 
-#include <kernel/time/hrtimer.h>
+#include <sched.h>
+
+#include <kernel/hrtimer.h>
+#include <kernel/kernel.h>
 
 #include "unit.h"
 
-volatile int counter;
+static struct hrtimer timer[5];
+static int count;
 
-static void callback(void *context)
+enum hrtimer_restart function(struct hrtimer *ht)
 {
-	static int expected = 0;
+	static unsigned int expect = 0;
 
-	if ((int)context != expected++)
+	if (ARRAY_INDEX(ht, timer) != expect++)
 		TEST_EXIT(1);
-	counter++;
+	count++;
+
+	return HRTIMER_NORESTART;
 }
 
 int main()
 {
-	struct hrtimer *timers[5];
-
 	for (int i = 0; i < 5; i++) {
-		timers[i] = hrtimer_alloc();
-		if (!timers[i])
-			TEST_EXIT(1);
-		timers[i]->callback = callback;
-		timers[i]->context = (void *)i;
+		hrtimer_init(&timer[i]);
+		timer[i].function = function;
 	}
 
 #define NSEC_PER_MSEC	1000000l
-	hrtimer_set_expires(timers[3],  900ll * NSEC_PER_MSEC); //  900 msecs
-	hrtimer_set_expires(timers[1],  500ll * NSEC_PER_MSEC); //  500 msecs
-	hrtimer_set_expires(timers[0],  250ll * NSEC_PER_MSEC); //  250 msecs
-	hrtimer_set_expires(timers[2],  700ll * NSEC_PER_MSEC); //  700 msecs
-	hrtimer_set_expires(timers[4], 1000ll * NSEC_PER_MSEC); // 1000 msecs
+	hrtimer_start(&timer[3],  900ll * NSEC_PER_MSEC); //  900 msecs
+	hrtimer_start(&timer[1],  500ll * NSEC_PER_MSEC); //  500 msecs
+	hrtimer_start(&timer[0],  250ll * NSEC_PER_MSEC); //  250 msecs
+	hrtimer_start(&timer[2],  700ll * NSEC_PER_MSEC); //  700 msecs
+	hrtimer_start(&timer[4], 1000ll * NSEC_PER_MSEC); // 1000 msecs
 
-	while (counter != 5)
-		;
+	while (count != 5)
+		sched_yield();
 
 	TEST_EXIT(0);
 }
